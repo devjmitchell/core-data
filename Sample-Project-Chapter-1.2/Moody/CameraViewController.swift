21a4e60ff3e138cb9eacf78ae6c1c52cb9377416
Jason Mitchell <devjmitchell@gmail.com>
2019-08-08 09:00:45 -0700

    Apply Xcode error, warning, and migration fixes

--- Sample-Project-Chapter-1.2/Moody/CameraViewController.swift
+++ Sample-Project-Chapter-1.2/Moody/CameraViewController.swift
@@ -91,8 +91,11 @@ extension CameraViewController: CaptureSessionDelegate {
 
 extension CameraViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
-        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+// Local variable inserted by Swift 4.2 migrator.
+let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+
+        if let image = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as? UIImage {
             delegate.didCapture(image)
         }
         dismiss(animated: true, completion: nil)
@@ -102,3 +105,13 @@ extension CameraViewController: UIImagePickerControllerDelegate, UINavigationCon
 }
 
 
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+	return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+	return input.rawValue
+}
